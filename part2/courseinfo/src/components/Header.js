3bfcf39cbb598a9c74975efbf20041aeeb497180
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-07-15 18:50:09 -0300

    2.3*: Course information — 8º step

--- part2/courseinfo/src/components/Header.js
+++ part2/courseinfo/src/components/Header.js
@@ -1,9 +1,14 @@
 const Header = ({ course }) => {
+    const parts = course.parts;
+    const total = parts.reduce((accumulator, currentValue) =>
+        accumulator + currentValue.exercises, 0
+    )
+
     return (
         <>
             <header><h1>{course.name}</h1></header>
-            {course.parts.map(part => <p key={part.id}>{part.name} <span>{part.exercises}</span></p>)}
-            <h4>total of {course.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)}</h4>
+            {parts.map(part => <p key={part.id}>{part.name} <span>{part.exercises}</span></p>)}
+            <h4>total of {total}</h4>
         </>
     )
 }
