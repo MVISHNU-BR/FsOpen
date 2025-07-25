dae6164de3c77f3720eaf8fb88cbc576deecde3f
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-07-25 20:02:23 -0300

    3.7: Phonebook backend — 7º step

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -3,7 +3,9 @@ const app = express()
 const morgan = require('morgan')
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', (request, response) => JSON.stringify(request.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
