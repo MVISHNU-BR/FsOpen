adf62ccd36055fc8f105850ff6789835b1226e5d
Vishnu Jneesh <mahajneesh@hotmail.com>
2025-09-08 18:02:11 -0300

    4.4: Helper functions and unit tests, step 2

--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogPosts) => {
+    let likes = 0;
+
+    for (const post of blogPosts) {
+        likes += post.likes;
+    }
+
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
