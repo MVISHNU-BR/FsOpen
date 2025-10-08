73f3a47b8e7681b01ab43079b2df63e2269430f8
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-10-07 21:55:17 -0300

    4.9: Tests for list Blog, step 2

--- part4/BlogList/tests/blog_api.test.js
+++ part4/BlogList/tests/blog_api.test.js
@@ -5,15 +5,29 @@ const app = require('../app');
 const api = supertest(app);
 
 
-test('return correct quantity of blog posts (0)', async () => {
-    await api
+test('return correct quantity of blog posts (if blogs db initiate with no blogs) (0)', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+
+    expect(response.body.length).toBe(8);
+}, 100000)
+
+
+test('if blogposts have a id propriety', async () => {
+    const response = await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-        .then(response => {
-            expect(response.body.length).toBe(0);
-        })
+
+    response.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+    });
+
 }, 100000)
+
+
 
 afterAll(async () => {
     await mongoose.connection.close()
