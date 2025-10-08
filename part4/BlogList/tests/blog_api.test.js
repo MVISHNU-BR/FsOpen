54e7ec0a4b76e713014c0205e7397d87ea82443f
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-10-07 22:51:20 -0300

    4.10: Tests for Blog List, step 3

--- part4/BlogList/tests/blog_api.test.js
+++ part4/BlogList/tests/blog_api.test.js
@@ -11,7 +11,7 @@ test('return correct quantity of blog posts (if blogs db initiate with no blogs)
         .expect(200)
         .expect('Content-Type', /application\/json/);
 
-    expect(response.body.length).toBe(8);
+    expect(response.body.length).toBe(11);
 }, 100000)
 
 
@@ -26,6 +26,34 @@ test('if blogposts have a id propriety', async () => {
     });
 
 }, 100000)
+
+test('post blog on api/blogs', async () => {
+    const blog = {
+        "title": "007 new Movies",
+        "author": "John Due",
+        "url": "oo7newMovies",
+        "likes": 3
+    }
+
+    const blogsBefore = (await api.get('/api/blogs').expect(200));
+    const numberOfBlogs = blogsBefore.body.length;
+
+    const response = await api
+        .post('/api/blogs')
+        .send(blog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+    const blogsAfter = (await api.get('/api/blogs').expect(200));
+    const numberOfBlogsAfterPost = blogsAfter.body.length
+
+    expect(numberOfBlogsAfterPost).toBe(numberOfBlogs + 1);
+    expect(response.body.title).toBe(blog.title);
+    expect(response.body.author).toBe(blog.author);
+    expect(response.body.url).toBe(blog.url);
+    expect(response.body.likes).toBe(blog.likes);
+
+})
 
 
 
