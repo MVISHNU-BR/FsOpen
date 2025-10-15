1b40eed38c0f8efec431392cb7bdca203c113932
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-10-15 19:04:51 -0300

    4.15: Blog list expansions, step 3

---
+++ part4/BlogList/controllers/users.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router();
+const User = require('../models/user');
+
+usersRouter.get('/', async (request, response, next) => {
+    try {
+        const usersOnDb = await User.find({})
+        response.json(usersOnDb)
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body;
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    const savedUser = await user.save();
+
+    response.status(201).json(savedUser);
+})
+
+
+
+module.exports = usersRouter
