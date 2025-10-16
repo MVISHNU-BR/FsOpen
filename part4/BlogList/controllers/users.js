e1122f126cac7c99e47c45317f43b5d9880953c5
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-10-16 19:11:24 -0300

    4.18: Blog list expansion, step 6

--- part4/BlogList/controllers/users.js
+++ part4/BlogList/controllers/users.js
@@ -11,32 +11,43 @@ usersRouter.get('/', async (request, response, next) => {
     }
 })
 
-usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body;
+usersRouter.post('/', async (request, response, next) => {
+    try {
+        const { username, name, password } = request.body;
 
-    if (!password || !username) {
-        return response.status(400).json({
-            error: "username and password are required"
+        if (!password || !username) {
+            return response.status(400).json({
+                error: "username and password are required"
+            })
+        }
+
+        if (password.length < 3) {
+            return response.status(400).json({ error: 'Password needs to be had 3 or more characters' })
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return response.status(400).json({
+                error: 'username must be unique'
+            });
+        }
+
+
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
+
+        const user = new User({
+            username,
+            name,
+            passwordHash
         })
+
+        const savedUser = await user.save();
+
+        response.status(201).json(savedUser);
+    } catch (exception) {
+        next(exception)
     }
-
-    if (password.length < 3) {
-        return response.status(400).json({ error: 'Password needs to be had 3 or more characters' })
-    }
-
-
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
-
-    const user = new User({
-        username,
-        name,
-        passwordHash
-    })
-
-    const savedUser = await user.save();
-
-    response.status(201).json(savedUser);
 })
 
 
